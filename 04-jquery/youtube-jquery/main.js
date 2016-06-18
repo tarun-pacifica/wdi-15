65f5a499d757837d0d7b2a1b26ba055bd0b13538
Christopher Barnaby <cjbarnaby@gmail.com>
2016-06-18 12:35:56 +1000

    more annotations for youtube-jquery

--- 04-jquery/youtube-jquery/main.js
+++ 04-jquery/youtube-jquery/main.js
@@ -1,3 +1,12 @@
+// I want to do a few things here:
+
+    // 1. I want to create thumbnails for the videos (CREATE, MANIPULATE, INJECT). I'll use the youtube library's 'generateThumbnailUrl' for this.
+    // 2. I want to add a youtube player to  my page. I'll make an iframe element for this (CREATE) and, back in my HTML file, I'll add a div in which to place my iframe.
+    // 3. Once I've create the iframe, I want to get the URL I need to play the video in the iframe and use that in the appropriate attribute of the iframe (MANIPULATE)
+    // 4. When a user clicks on a video (EVENT HANDLER), I want that video to be displayed in my iframe (INJECT).
+
+// To follow the logic of this code through, you'll need to jump down to line 41 - that's where it all starts.
+
 
 // Create a function to generate thumbnails for YouTube links.
 var thumbnailify = function ($videoLink) {
@@ -9,11 +18,11 @@ var thumbnailify = function ($videoLink) {
 
     // Add an event handler that listens for the 'click' event on the element
     $videoLink.on('click', function (event) {
-        // By default, the browser will try to open the YouTube URL. We want to "PREVENT" that "DEFAULT" behaviour.
+        // Since our video thumbnails are going to be INSIDE an <a> tag, if we click a thumbnail, by default the browser will try to open the YouTube URL. But we don't wnt that to happen - we want the video to be displayed in our video player. So, we want to "PREVENT" that "DEFAULT" behaviour.
         event.preventDefault();
-        // Instead, we want to create a new iframe element.
+        // And instead, we want to create a new iframe element for our player.
         var videoEmbed = $('<iframe>');
-        // And get the embed URL for the video using the generateEmbedUrl method from the youtube JS library.
+        // Then get the embed URL for the video using the generateEmbedUrl method from the youtube JS library.
         videoEmbed.attr( 'src', youtube.generateEmbedUrl(linkUrl) );
         // Set the height and widths of the iframe element
         videoEmbed.attr( 'width', 500 );
