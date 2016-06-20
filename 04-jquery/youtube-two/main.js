8c09dbdaa16056a5ee48cba47452cc1840a9a101
Christopher Barnaby <cjbarnaby@gmail.com>
2016-06-20 13:38:26 +1000

    annots to jquery-two

--- 04-jquery/youtube-two/main.js
+++ 04-jquery/youtube-two/main.js
@@ -1,26 +1,40 @@
 
+// I've left the regular JS DOM API code in as comments above my fancy jQuery.
+
 // Create the array of links
+// var linkTags = document.querySelectorAll('a');
 var $linkTags = $("a");
 
 // Iterate through the array of links
+// for (var i = 0; i < linkTags.length; i++) {
 $linkTags.each(function() {
-    var $link = $(this);
-    var linkUrl = $link.attr("href");
-    var thumbnailUrl = youtube.generateThumbnailUrl(linkUrl);
-    var $thumbnailImage = $("<img>").attr("src", thumbnailUrl);
-    $link.append($thumbnailImage);
-    // UN-sexy one-line version:
-    // $(this).append($("<img>").attr("src", youtube.generateThumbnailUrl($(this).attr("href"))));
+
+        // Store the current link in a variable
+        // var link = linkTags[i];
+        var $link = $(this);
+
+        // Get the current value of the href attribute of that link
+        // var linkUrl = link.getAttribute('href');
+        var linkUrl = $link.attr("href");
+
+        // Use the youtube javasript library to generate a thumbnail using that href
+        // var thumbnailUrl = youtube.generateThumbnailUrl(linkUrl);
+        var thumbnailUrl = youtube.generateThumbnailUrl(linkUrl);
+
+        // The next two statements we're going to chain together...
+        // Create an image and store the object in a variable
+        // var thumbnailImage = document.createElement('img');
+        // Set that image's src attribute
+        // thumbnailImage.setAttribute('src', thumbnailUrl);
+        var $thumbnailImage = $("<img>").attr("src", thumbnailUrl);
+
+        // Append that image as a child of the current link in the collection.
+        // link.appendChild(thumbnailImage);
+        $link.append($thumbnailImage);
+// };
+
+// NOTE: We could have written the entire block in this one liner, but it's extremely difficult to follow what's going on:
+
+        // $(this).append($("<img>").attr("src", youtube.generateThumbnailUrl($(this).attr("href"))));
+
 });
-
-
-
-// Less un-sexy for-loop that doesn't take advantage of jQuery's .each().
-// for (var i = 0; i < $linkTags.length; i++) {
-//     var $link = $linkTags.eq(i);
-//     var linkUrl = $link.attr("href");
-//     var thumbnailUrl = youtube.generateThumbnailUrl(linkUrl);
-//     var $thumbnailImage = $("<img>");
-//     $thumbnailImage.attr("src", thumbnailUrl);
-//     $link.append($thumbnailImage);
-// }
