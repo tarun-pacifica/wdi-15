341c0ddeb75291ed7b5a006eb5dc36c54d7f1f37
William of Ockham <certainthings@gmail.com>
2016-07-07 12:56:04 +1000

    moma associations

--- 08-rails/moma/db/schema.rb
+++ 08-rails/moma/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706020958) do
+ActiveRecord::Schema.define(version: 20160707022058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160706020958) do
     t.text     "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "artist_id"
   end
 
 end
