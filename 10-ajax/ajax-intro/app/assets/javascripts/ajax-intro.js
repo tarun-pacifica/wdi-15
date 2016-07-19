19c09338c826167e3a3159fd21c4d858360ae5e4
William of Ockham <certainthings@gmail.com>
2016-07-19 12:38:05 +1000

    jQuery AJAX

--- 10-ajax/ajax-intro/app/assets/javascripts/ajax-intro.js
+++ 10-ajax/ajax-intro/app/assets/javascripts/ajax-intro.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
   $('#fetchLotto').on('click', function () {
-    var xhr = new XMLHttpRequest(); // Instantiate our AJAX request object.
-    xhr.open('GET', '/lotto'); // Set up our request: method and the URL or path
-    xhr.onreadystatechange = function () { // Set up our callback
-      if (xhr.readyState !== 4) { return; } // Don't do anything until the request is complete
 
-      // Good to go!
-      $('#lottery').append( xhr.responseText + ', ' );
-    };
-    xhr.send();
+    $.ajax('/lotto').done(function (number) {
+      $('#lottery').append(number + ', ');
+    })
+
+    // var xhr = new XMLHttpRequest(); // Instantiate our AJAX request object.
+    // xhr.open('GET', '/lotto'); // Set up our request: method and the URL or path
+    // xhr.onreadystatechange = function () { // Set up our callback
+    //   if (xhr.readyState !== 4) { return; } // Don't do anything until the request is complete
+    //
+    //   // Good to go!
+    //   $('#lottery').append( xhr.responseText + ', ' );
+    // };
+    // xhr.send();
   });
 });
