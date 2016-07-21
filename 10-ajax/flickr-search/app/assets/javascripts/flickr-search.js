0396ef6cea7d4ca2bfc3d713d8cd4e55bce1a920
William of Ockham <certainthings@gmail.com>
2016-07-21 11:20:38 +1000

    requestInProgress flag

--- 10-ajax/flickr-search/app/assets/javascripts/flickr-search.js
+++ 10-ajax/flickr-search/app/assets/javascripts/flickr-search.js
@@ -1,23 +1,42 @@
 $(document).ready(function () {
+
+  var settings = {
+    currentPage: 1,
+    endReached: false,
+    requestInProgress: false
+  };
+
   var searchFlickr = function () {
+
+    console.log(settings);
+
+    if (settings.endReached) { return; }
+
     var query = $('#query').val();
 
     var flickrURL = 'https://api.flickr.com/services/rest/?jsoncallback=?';
 
-    console.log('searching flickr for', query);
+    settings.requestInProgress = true;
+
+    console.log('searching flickr for', query, 'current page', settings.currentPage);
 
     $.getJSON(flickrURL, {
       method: 'flickr.photos.search',
       api_key: '2f5ac274ecfac5a455f38745704ad084',
       text: query,
-      format: 'json'
+      format: 'json',
+      page: settings.currentPage++
     }).done(function (info) {
+      if (info.photos.page >= info.photos.pages) {
+        settings.endReached = true;
+      }
       _.each(info.photos.photo, function (p) {
         var url = generateURL(p);
         var $img = $('<img>', {
           src: url
         }).appendTo('#results');
       });
+      settings.requestInProgress = false;
     });
   };
 
@@ -35,14 +54,20 @@ $(document).ready(function () {
     ].join('');
   };
 
-  $('#search').on('click', searchFlickr);
+  $('#search').on('click', function () {
+    settings.endReached = false;
+    settings.currentPage = 1;
+    $('#results').empty();
+    searchFlickr();
+  });
 
   $(window).on('scroll', function () {
     var scrollTop = $(window).scrollTop();
     var windowHeight = $(window).height();
     var documentHeight = $(document).height();
 
-    if (scrollTop + windowHeight > 0.8 * documentHeight) {
+    if (settings.requestInProgress === false &&
+        scrollTop + windowHeight > 0.8 * documentHeight) {
       searchFlickr();
     }
 
