53b526772c604d3c4c4af73787952c3497ba83d7
William of Ockham <certainthings@gmail.com>
2016-08-17 16:05:00 +1000

    recursion

--- 13-advanced/recursion/fibonacci.rb
+++ 13-advanced/recursion/fibonacci.rb
@@ -8,4 +8,27 @@ def fib_i(n)
   b
 end
 
-puts fib_i 7
+def fib_r_slow(n)
+  # Memoization would make this more efficient
+  if n <= 2
+    1
+  else
+    fib_r_slow(n - 1) + fib_r_slow(n - 2)
+  end
+end
+
+def fib_r_fast(n, a=1, b=1)
+  if n > 2
+    fib_r_fast(n - 1, b, a + b)
+  else
+    b
+  end
+
+end
+
+100_000.times do |i|
+  puts "fibonacci no #{ i } "
+  puts "iterative: #{ fib_i i }"
+  puts "recursive #{ fib_r_fast i }"
+  puts "*" * 80
+end
